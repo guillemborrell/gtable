215299ef178873637c39590aa6f322457a3884e1
Guillem Borrell <guillemborrell@gmail.com>
2017-08-02 22:00:26 +0200

    Added simpe tests

---
+++ tests/test_table_creation.py
@@ -0,0 +1,70 @@
+from gtable import Table
+import numpy as np
+import pytest
+
+
+def test_empty_table():
+    t = Table()
+    assert t.data == {}
+
+def test_simple_table():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+
+    assert t.data['a'][2] == 3
+    assert t._indexes == {'a': 0, 'b': 0}
+    assert len(t._index_store) == 1
+    
+def test_hcat_table():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+    t.hcat('c', [7,8,9])
+
+    assert t._indexes == {'a': 0, 'b': 0, 'c': 0}
+    assert np.all(t.c.values == np.array([7,8,9]))
+
+def test_vcat_table():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+    t1 = Table({'a': [1,2,3], 'd': np.array([4,5,6])})
+    t.vcat(t1)
+
+    assert np.all(t._index_store[0] == np.array([0, 1, 2]))
+    assert np.all(t._index_store[1] == np.array([0, 1, 2, 3, 4, 5]))
+    assert np.all(t._index_store[2] == np.array([3, 4, 5]))
+    assert t._indexes == {'a': 1, 'b': 0, 'd': 2}    
+
+def test_records():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+    t1 = Table({'a': [1,2,3], 'd': np.array([4,5,6])})
+    t.vcat(t1)
+
+    records = [r for r in t.records()]
+    assert records == [
+        {'a': 1, 'b': 4},
+        {'a': 2, 'b': 5},
+        {'a': 3, 'b': 6},
+        {'a': 1, 'd': 4},
+        {'a': 2, 'd': 5},
+        {'a': 3, 'd': 6}]
+
+def test_compute_column():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+    t1 = Table({'a': [1,2,3], 'd': np.array([4,5,6])})
+    t.vcat(t1)
+
+    t.c = t.a + t.a/2
+    records = [r for r in t.records()]
+    assert records == [
+        {'a': 1, 'b': 4, 'c': 1.5},
+        {'a': 2, 'b': 5, 'c': 3.0},
+        {'a': 3, 'b': 6, 'c': 4.5},
+        {'a': 1, 'd': 4, 'c': 1.5},
+        {'a': 2, 'd': 5, 'c': 3.0},
+        {'a': 3, 'd': 6, 'c': 4.5}]
+
+def test_compute_wrong_size():
+    t = Table({'a': [1,2,3], 'b': np.array([4,5,6])})
+    t1 = Table({'a': [1,2,3], 'd': np.array([4,5,6])})
+    t.vcat(t1)
+
+    with pytest.raises(ValueError) as excinfo:
+        t.c = t.a + t.b/2
+    assert 'broadcast together' in str(excinfo.value)
