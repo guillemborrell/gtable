8e894ebd8c4bf76dce5027e915a2824f7ecc1e9e
Guillem Borrell <guillemborrell@gmail.com>
2017-08-08 19:51:11 +0200

    Slightly better performance with a fast-assignment mechanism

--- gtable.py
+++ gtable.py
@@ -3,7 +3,6 @@ import pandas as pd
 import sys
 from collections import OrderedDict
 from itertools import chain
-from numba import jit
 
 
 class Column:
@@ -67,48 +66,29 @@ class Column:
         elif type(y) == Column:
             return Column(self.values ** y.values, self.index)
 
-        
+
+def _check_length(i, k, this_length, length_last):
+    if i == 0:
+        length_last = this_length
+    else:
+        if this_length != length_last:
+            raise ValueError("Column {} length mismatch".format(k))
+        else:
+            length_last = this_length
+
+    return length_last
+
+
 class Table:
     """
     Table is a class for fast columnar storage using a bitmap index for
     sparse storage
     """
-    def copy(self):
-        t = Table()
-        t._data = OrderedDict([(k, v.copy()) for k, v in self._data.items()])
-        t._index = self._index.copy()
-        if self._order is not None:
-            t._order = self._order.copy()
-
-        return t
-    
-    @staticmethod
-    def _check_length(i, k, this_length, length_last):
-        if i == 0:
-            length_last = this_length
-        else:
-            if this_length != length_last:
-                raise ValueError("Column {} length mismatch".format(k))
-            else:
-                length_last = this_length
-
-        return length_last
-
-    def _index_column(self, key):
-        # This only works with OrderedDicts
-        for i, k in enumerate(self._data):
-            if key == k:
-                return self._index[i, :]
-
-    def _index_index(self, key):
-        # This only works with OrderedDicts
-        for i, k in enumerate(self._data):
-            if key == k:
-                return i
-
     def __init__(self, data={}):
-        # This dictionary stores the columns
-        self._data = OrderedDict()
+        # This list stores the keys
+        self._keys = []
+        # This list stores the columns
+        self._data = []
         # This is the index bitmap
         self._index = None
         # This is the order of the table
@@ -119,29 +99,46 @@ class Table:
         for i, (k, v) in enumerate(data.items()):
             # If the column is a list, cast it to a numpy array
             if type(v) == list:
-                self._data[k] = np.array(v)
-                length_last = self._check_length(i, k, len(v), length_last)
+                self._data.append(np.array(v))
+                self._keys.append(k)
+                length_last = _check_length(i, k, len(v), length_last)
                     
             elif type(v) == np.ndarray:
                 if not len(v.shape) == 1:
                     raise ValueError("Only 1D arrays supported")
-                self._data[k] = v
-                length_last = self._check_length(i, k, v.shape[0], length_last)
+                self._data.append(v)
+                self._keys.append(k)
+                length_last = _check_length(i, k, v.shape[0], length_last)
 
             # Pandas DatetimeIndex is supported for convenience.
             elif type(v) == pd.DatetimeIndex:
-                self._data[k] = v
-                length_last = self._check_length(i, k, v.shape[0], length_last)
+                self._data.append(v)
+                self._keys.append(k)
+                length_last = _check_length(i, k, v.shape[0], length_last)
                 
             else:
                 raise ValueError("Column type not supported")
 
         # Create the index and the ordered arrays
         self._index = np.ones((len(data), length_last), dtype=np.uint8)
-            
+        
+    def copy(self):
+        t = Table()
+        t._data = [d.copy() for d in self._data]
+        t._keys = self._keys[:]
+        t._index = self._index.copy()
+        
+        if self._order is not None:
+            t._order = self._order.copy()
+
+        return t
+    
+    def _index_column(self, key):
+        return self._index[self._keys.index(key), :]
+
     def __repr__(self):
         column_info = list()
-        for k, v in self._data.items():
+        for k, v in zip(self._keys, self._data):
             if type(v) == np.ndarray:
                 column_type = v.dtype
             else:
@@ -152,25 +149,34 @@ class Table:
         return "<Table[ {} ] object at {}>".format(', '.join(column_info),hex(id(self)))
     
     def __getattr__(self, key):
-        return Column(self._data[key], self._index_column(key))
+        return Column(self._data[self._keys.index(key)], self._index_column(key))
 
-     def hcat(self, k, v, index=None):
+    def __getitem__(self, key):
+        return self._data[self._keys.index(key)]
+
+    def __setitem__(self, key, value):
+        self._data[self._keys.index(key)] = value
+
+    def hcat(self, k, v, index=None):
         """
         Column concatenation.
         """
-        if k in self._data:
+        if k in self._keys:
             raise KeyError("Key {} already present".format(k))
 
         if type(v) == list:
-            self._data[k] = np.array(v)
+            self._data.append(np.array(v))
+            self._keys.append(k)
                     
         elif type(v) == np.ndarray:
             if not len(v.shape) == 1:
                 raise ValueError("Only 1D arrays supported")
-            self._data[k] = v
+            self._data.append(v)
+            self._keys.append(k)
 
         elif type(v) == pd.DatetimeIndex:
-            self._data[k] = v
+            self._data.append(v)
+            self._data.append(k)
                 
         else:
             raise ValueError("Column type not supported")
@@ -198,7 +204,7 @@ class Table:
                     [self._index,
                      np.zeros(
                          (self._index.shape[0],
-                        index.shape[0] - self._index.shape[0]),
+                          index.shape[0] - self._index.shape[0]),
                          dtype=np.uint8)],
                     axis=1
                 )
@@ -209,14 +215,14 @@ class Table:
     def vcat(self, table):
         """Vertical (Table) concatenation."""
         # First step is to rearrange the bitmap index if needed
-        joined_columns = set(chain(self._data, table._data))
+        joined_columns = set(chain(self._keys, table._keys))
         hspill = len(joined_columns) - self._index.shape[0]
         before_growth = self._index.shape
 
         tindex = table._index.copy()
         
         # Add the horizontal spill (more columns)
-        if joined_columns != set(self._data):
+        if joined_columns != set(self._keys):
             self._index = np.concatenate(
                 [self._index, np.zeros(
                     (hspill, self._index.shape[1]), dtype=np.uint8
@@ -229,29 +235,33 @@ class Table:
             )], axis=1)
 
         # Include the keys present in both tables with this light nested loop.
-        for i, old_key in enumerate(self._data):
-            for j, new_key in enumerate(table._data):
+        for old_key in self._keys:
+            for new_key in table._keys:
                 if new_key == old_key:
-                    self._index[i, before_growth[1]:] = tindex[j, :]
-                    self._data[old_key] = np.concatenate(
-                        [self._data[old_key], table._data[new_key]]
-                        )
+                    self._index[self._keys.index(old_key),
+                                before_growth[1]:] = tindex[
+                                    table._keys.index(new_key), :]
+                    self._data[self._keys.index(old_key)] = np.concatenate(
+                        [self._data[self._keys.index(old_key)],
+                         table._data[table._keys.index(new_key)]]
+                    )
 
         # Include keys that are not added in the previous table
         new_cols_added = 0
-        for i, new_key in enumerate(table._data):
-            if new_key not in self._data:
+        for new_key in table._keys:
+            if new_key not in self._keys:
                 self._index[before_growth[0] + new_cols_added,
-                            before_growth[1]:] = tindex[i, :]
-                self._data[new_key] = table._data[new_key]
+                            before_growth[1]:] = tindex[
+                                table._keys.index(new_key), :]
+                self._data.append(table._data[table._keys.index(new_key)])
+                self._keys.append(new_key)
                 new_cols_added += 1
-
                 
     def records(self, fill=False):
         """Generator that returns a dictionary for each row of the table"""
         # Infinite counter. SLOOOOOOW. This is columnar storage.
         counters = np.zeros((self._index.shape[0]), dtype=np.int)
-        keys = np.array([k for k in self._data])
+        keys = np.array(self._keys)
         
         if self._order is not None:
             for record in self._index.T[self._order]:
@@ -259,7 +269,8 @@ class Table:
                 selected_counters = counters[np.where(record)]
                 selected_values = list()
                 for k, c in zip(selected_keys, selected_counters):
-                    selected_values.append(self._data[k][c])
+                    idx = self._keys.index(k)
+                    selected_values.append(self._data[idx][c])
                 counters[np.where(record)] += 1
             
                 if fill:
@@ -274,8 +285,9 @@ class Table:
                 selected_keys = keys[np.where(record)]
                 selected_counters = counters[np.where(record)]
                 selected_values = list()
+
                 for k, c in zip(selected_keys, selected_counters):
-                    selected_values.append(self._data[k][c])
+                    selected_values.append(self._data[self._keys.index(k)][c])
                 counters[np.where(record)] += 1
             
                 if fill:
@@ -292,15 +304,15 @@ class Table:
             self.__dict__[key] = value
         else:
             if type(value) == Column:
-                if key in self._data:
-                    self._data[key] = value.values
-                    self._index[self._index_index(key), :] = value.index
+                if key in self._keys:
+                    self._data[self._keys.index(key)] = value.values
+                    self._index[self._keys.index(key), :] = value.index
                 else:
                     self.hcat(key, value.values, value.index)
 
             elif type(value) == np.ndarray:
-                if key in self._data:
-                    self._data[key] = value
+                if key in self._keys:
+                    self._data[self._keys.index(key)] = value
                 else:
                     self.hcat(key, value)
 
@@ -309,7 +321,7 @@ class Table:
     
     @property
     def data(self):
-        return self._data
+        return {k: v for k, v in zip(self._keys, self._data)}
 
     @property
     def index(self):
