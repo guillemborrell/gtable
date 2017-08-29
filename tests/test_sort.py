6b33e0e39b107a60bc0343b71db4e635c3ad8ffa
Guillem Borrell <guillemborrell@gmail.com>
2017-08-29 14:18:01 +0200

    Added a trivial test for sorting.

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -3,7 +3,19 @@ from gtable import Table
 import numpy as np
 
 
-def test_sort():
+def test_sort_0():
+    a = [1, 2, 3, 4, 5]
+    b = [5, 4, 3, 2, 1]
+
+    table = Table({'a': a, 'b': b})
+
+    table.sort_by('b')
+
+    assert np.all(table.a.values == np.array([5, 4, 3, 2, 1]))
+    assert np.all(table.b.values == np.array([1, 2, 3, 4, 5]))
+
+
+def test_sort_1():
     a = [0, 1, 1, 5, 3, 2]
     b = [0, 1, 2, 3, 4, 5]
     table = Table({'a': a, 'b': b})
@@ -15,6 +27,6 @@ def test_sort():
 
     assert np.all(table.a.values == np.array([0, 1, 1, 2, 3, 5]))
     assert np.all(table.b.values == np.array(
-        [0, 1, 2, 5, 4, 3, 1,  2, 3, 4]
+        [0, 1, 2, 5, 4, 3, 1, 2, 3, 4]
     ))
 
